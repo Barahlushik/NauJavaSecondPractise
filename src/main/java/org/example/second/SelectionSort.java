007e592d24a8d09739b0db8dab7048b47b177bba
Mark <marik.kriger@yandex.ru>
2026-03-23 19:26:30 +0300

    Исправлеие второго задания: замена оператора сравнения на compareTo для избежания распаковки в алгоритме сортировки

--- src/main/java/org/example/second/SelectionSort.java
+++ src/main/java/org/example/second/SelectionSort.java
@@ -8,7 +8,7 @@ public class SelectionSort {
         for (int i = 0; i < list.size() - 1; i++) {
             int min = i;
             for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(j) < list.get(min)) {
+                if (Double.compare(list.get(j), list.get(min)) < 0) {
                     min = j;
                 }
             }
